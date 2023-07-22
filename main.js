cd2bab0a606da41bd93c656e54f1d23d3f137080
Md. Rabiul Islam <91551048+rabiul121@users.noreply.github.com>
2023-07-22 17:17:51 +0600

    completed calculator project

--- main.js
+++ main.js
@@ -19,8 +19,80 @@ document.addEventListener("DOMContentLoaded", function (event) {
         currentScreen.textContent = currentValue;
     }))
 
+    operators.forEach((op) => op.addEventListener("click", function (e) {
+        handleOperator(e.target.textContent);
+        previousScreen.textContent = previousValue + " " + operator;
+        currentScreen.textContent = currentValue;
+
+    }))
+
+    clear.addEventListener("click", function () {
+        previousValue = '';
+        currentValue = '';
+        operator = '';
+        previousScreen.textContent = currentValue;
+        currentScreen.textContent = currentValue;
+    })
+
+    equal.addEventListener("click", function () {
+        if (currentValue != '' && previousValue != '') {
+            calculate();
+            previousScreen.textContent = '';
+            if (previousValue.length <= 14) {
+                currentScreen.textContent = previousValue;
+            } else {
+                currentScreen.textContent = previousValue.slice(0, 14) + "...";
+            }
+        }
+    })
+
+    decimal.addEventListener("click", function () {
+        addDecimal();
+        currentScreen.textContent = currentValue;
+    })
+
+
     function handleNumber(num) {
-        currentValue += num;
+        if (currentValue.length <= 15) {
+            currentValue += num;
+        }
+    }
+
+    function handleOperator(op) {
+        operator = op;
+        previousValue = currentValue;
+        currentValue = '';
     }
 
 });
+
+
+function calculate() {
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+    if (operator === '+') {
+        previousValue += currentValue;
+    }
+    else if (operator === '-') {
+        previousValue -= currentValue;
+    } else if (operator === '*') {
+        previousValue *= currentValue;
+    } else if (operator === '/') {
+        previousValue /= currentValue;
+    }
+
+    previousValue = roundNumber(previousValue);
+    previousValue = previousValue.toString();
+    currentValue = previousValue.toString();
+}
+
+
+function roundNumber(num) {
+    return Math.round(num * 1000) / 1000;
+}
+
+function addDecimal() {
+    if (!currentValue.includes(".")) {
+        currentValue += ".";
+    }
+}
